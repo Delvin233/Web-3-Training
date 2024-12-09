a9fddd2f6e54bf16feea725615bed548759453f0
Delvin233 <yamoahdelvin@gmail.com>
2024-12-09 18:56:09 +0000

    ether units

---
+++ Cook_Web3/Beginner_Chef/bits and units/ether units/ether-units.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >0.7.0 <=0.9.0;
+
+
+// etherium is the blockchain and ether is the currency
+// wei is the smallest denomination of ether
+// 1 eth = 10^18 wei
+contract etherUnits{
+    // Assert should only be used to test for internal errors, and to check invariants. 
+    function learnUnits() public pure{
+        assert(1 ether == 1e18 wei);
+    }
+    // assert times and dates in solidity
+    function assertTime() public pure{
+        assert (60 second = 1 minute );
+    }
+}
